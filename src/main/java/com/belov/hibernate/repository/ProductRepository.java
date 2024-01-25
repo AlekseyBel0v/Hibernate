fa92a2ee0ff4de8ab4df33a4623f87ea974ff7b4
AlexeyBelov <bel0valeksey@yandex.ru>
2024-01-25 19:31:55 +0300

    task by 2 tables with @ManyToOne - add coalesce()

--- src/main/java/com/belov/hibernate/repository/ProductRepository.java
+++ src/main/java/com/belov/hibernate/repository/ProductRepository.java
@@ -1,10 +1,8 @@
 package com.belov.hibernate.repository;
 
-import com.belov.hibernate.domain.Order;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
-import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -21,11 +19,13 @@ public class ProductRepository {
     public List<String> getProductName(String customerName) {
         List<String> products = new ArrayList<>();
         Query query = entityManager.createQuery(
-                "select o.productName, count(o.amount) from Order o where customer.name ilike :customerName group by o.productName");
+                "select o.productName, " +
+                        //coalesce меняет null на 0
+                        "sum(coalesce(o.amount, 0.0))  from Order o where customer.name ilike :customerName group by o.productName");
         query.setParameter("customerName", customerName);
         List<Object[]> objects = query.getResultList();
         for (Object[] o : objects) {
-            products.add((String) o[0] + (Long) o[1]);
+            products.add(String.format("%s - %f", (String) o[0], (Double) o[1]));
         }
         return products;
     }
